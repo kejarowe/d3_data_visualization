a100bb2e4546295343d585499479a03d113a70c7
Keja Rowe <keja.rowe@gmail.com>
2016-07-09 17:30:47 -0400

    added aggregated data file

--- data_filter.R
+++ data_filter.R
@@ -1,7 +1,7 @@
 library(ggplot2)
-
+#library(plyr)
 #read in raw data
-airline_data_2008 <- read.csv("~/data_analyst_nd/p6/final/airline_data_2008.csv")
+airline_data_2008 <- read.csv("~/data_analyst_nd/p6/final/data/airline_data_2008.csv")
 
 #delete all unneeded columns
 airline_data_filtered <- airline_data_2008[,c("UniqueCarrier","DepDelay")]
@@ -15,5 +15,15 @@ rm(airline_data_2008)
 #inspect the data using quick plots
 qplot(x = UniqueCarrier, data = airline_data_filtered)
 
+#aggrigate data
+#data_agg <- aggregate(airline_data_filtered$DepDelay,
+#                      by=list(airline_data_filtered$UniqueCarrier),
+#                      FUN = function(x) c(total_flights = length(x), total_delays = sum(x) ) )
+
+data_agg <- aggregate(. ~ UniqueCarrier, data = airline_data_filtered,
+                      FUN = function(x) c(total_flights = length(x), total_delays = sum(x) ) )
+
+
 #save filtered dataframe to CSV
-write.csv(airline_data_filtered,"~/data_analyst_nd/p6/final/filtered_airline_data.csv",quote=FALSE,row.names=FALSE)
+write.csv(airline_data_filtered,"~/data_analyst_nd/p6/final/data/filtered_airline_data.csv",quote=FALSE,row.names=FALSE)
+write.csv(data_agg,"~/data_analyst_nd/p6/final/data/aggregated_airline_data.csv",quote=FALSE,row.names=FALSE)
